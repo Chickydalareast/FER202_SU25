a484910976835edd1a9dc336c011953d961d709f
Chicky <phantrunghieu3101@gmail.com>
2025-06-18 14:34:58 +0700

    Revert "Merge pull request #2 from Chickydalareast/hieptt/fixfooter"
    This reverts commit 1842c99f1be7cc8603cb360e91ca7a05ef6848e1, reversing
    changes made to a343df7a47929f0c2e8b87c06a938c5e0e534d7a.

--- src/Constants/AppConfig.js
+++ src/Constants/AppConfig.js
@@ -1,28 +1,7 @@
-import applogo from "../Assets/image/logo122.png";
-export const APP_CONFIG = Object.freeze({
-  APP_NAME: "ELearning",
-  APP_SLOGAN: "Học Tập Vì Tương Lai",
-  APP_LOGO: applogo,
-  DIA_CHI: "Đại học FPT, Ngũ Hành Sơn, Thành Phố Đà Nẵng",
-  EMAIL: "...@gmail.com.vn",
-  PHONE_NUMBER: "08 1919 0909",
-  CONG_CU: [
-    { label: "React", link: "https://react.dev/" },
-    { label: "NodeJS", link: "https://nodejs.org/en" },
-    { label: "MongoDB", link: "https://www.mongodb.com/" },
-    { label: "ExpressJS", link: "https://expressjs.com/" },
-    { label: "CSS Taiwind", link: "https://tailwindcss.com/" },
-  ],
-  PRODUCT: [
-    {
-      label: "Website Khách Sạn",
-      link: "https://booking.blwsmartware.net/",
-    },
-  ],
-  ABOUT: [
-    { label: "Giới thiệu", link: "#" },
-    { label: "Liên hệ", link: "#" },
-    { label: "Điều khoản", link: "#" },
-    { label: "Bảo mật", link: "#" },
-  ],
+
+import applogo from"../Assets/image/logo122.png"
+export const APP_CONFIG =Object.freeze({
+    APP_NAME:'ELearning',
+    APP_SLOGAN:'Học Tập Vì Tương Lai',
+    APP_LOGO:applogo,
 });
